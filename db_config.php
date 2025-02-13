ab95b6a403fd4681515dc587abba903258d84c62
Hachi1228 <marcxd21@gmail.com>
2025-02-13 16:51:32 +0800

    ONLINE DB CONNECTION

--- db_config.php
+++ db_config.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "humanresource";
+$servername = "sql301.infinityfree.com";
+$username = "if0_38306620";
+$password = "8eu4xCWPEtqGRY";
+$dbname = "if0_38306620_humanresource";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
